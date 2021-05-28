a41d25fdd4a853dca2bfd1aff388cbc355dd2c51
Roland Hummel <roland.hummel@qaware.de>
2021-05-28 23:04:26 +0200

    Fix HMR for css and app unmounting at the same time

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import { createApp } from "vue";
 import App from "./App";
 
 const app = createApp(App);
-
 app.mount("#root");
 
 if (import.meta.hot) {
     import.meta.hot.accept();
-    import.meta.hot.dispose(() => {
-        app.$destroy();
-    });
+    import.meta.hot.dispose(() => app.unmount());
 }
