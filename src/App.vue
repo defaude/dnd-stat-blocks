d41ca3413e09fb0023b5aba153fd9ab260d96e9f
Roland Hummel <roland.hummel@qaware.de>
2021-05-28 22:46:24 +0200

    switch to using TS, move stuff around a bit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>hello, world</h1>
+  <p>color: {{ statBlock.color }}</p>
+</template>
+
+<script lang="ts">
+import { StatBlock } from "./types";
+
+export default {
+  setup() {
+    const statBlock: StatBlock = {
+      color: "red"
+    };
+
+    return {
+      statBlock
+    };
+  }
+}
+</script>
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type StatBlock = {
+    color: string;
+};
