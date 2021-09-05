8b18cac2d24213803bff27bf6a77d983e627f81b
Roland Hummel <roland.hummel@qaware.de>
2021-09-05 19:21:44 +0200

    Stat block data: Evil Mage

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,51 @@
 </template>
 
 <script lang="ts">
-import {StatBlockData} from "./types";
-import StatBlock from "./StatBlock.vue";
+import { StatBlockData } from './types';
+import StatBlock from './StatBlock.vue';
 
 export default {
-  components: {StatBlock},
+  components: { StatBlock },
 
   setup() {
     const statBlock: StatBlockData = {
-      title: "Kréa (Fox)",
-      description: "Tiny beast, unaligned",
+      title: 'Evil Mage',
+      description: 'Medium humanoid (human), lawful evil',
       primaryStats: [
-        {name: "Armor Class", value: "13"},
-        {name: "Hit Points", value: "5 (2d4)"},
-        {name: "Speed", value: "30 ft., burrow 5ft."}
-      ],
-      secondaryStats: [
-        {name: "Skills", value: "Perception +3, Stealth +5"},
-        {name: "Senses", value: "Darkvision 60ft., passive Perception 13"},
-        {name: "Languages", value: "none"},
-        {name: "Challenge", value: "1/8 (10 XP)"}
+        { name: 'Armor Class', value: '12' },
+        { name: 'Hit Points', value: '22 (5d8)' },
+        { name: 'Speed', value: '30 ft.' }
       ],
       attributes: [
-        {attribute: "STR", value: 2},
-        {attribute: "DEX", value: 16},
-        {attribute: "CON", value: 11},
-        {attribute: "INT", value: 3},
-        {attribute: "WIS", value: 13},
-        {attribute: "CHA", value: 10}
+        { attribute: 'STR', value: 9 },
+        { attribute: 'DEX', value: 14 },
+        { attribute: 'CON', value: 11 },
+        { attribute: 'INT', value: 17 },
+        { attribute: 'WIS', value: 12 },
+        { attribute: 'CHA', value: 11 }
+      ],
+      secondaryStats: [
+        { name: 'Saving throws', value: 'Int +5, Wis +3' },
+        { name: 'Skills', value: 'Arcana +5, History +5' },
+        { name: 'Senses', value: 'passive Perception 11' },
+        { name: 'Languages', value: 'Common, Draconic, Dwarvish, Elvish' },
+        { name: 'Challenge', value: '1 (200 XP)' }
       ],
       blocks: [
         {
           texts: [
-            '***Keen Hearing.*** The fox has advantage on Wisdom (Perception) checks that rely on hearing.'
+            '***Spellcasting.*** The mage is a 4th-level spellcaster that uses Intelligence as its spellcasting ability (spell save DC 13; +5 to hit with spell attacks). The mage knows the following spells from the wizard\'s spell list:\n- Cantrips (at will): light, mage hand, shocking grasp\n- 1st Level (4 slots): *charm person, magic missile*\n- 2nd Level (3 slots): *hold person, misty step*'
           ]
         },
         {
           heading: 'Actions',
           texts: [
-            '***Bite.*** *Melee weapon attack:* +5 to hit, react 5 ft., one creature. *Hit:* 1d4 piercing damage.'
+            '***Quarterstaff.*** *Melee Weapon Attack:* +1 to hit, reach 5 ft., one target. *Hit:* 3 (1d8-1) bludgeoning damage.',
+          ]
+        },
+        {
+          texts: [
+              'Evil mages hunger for arcane power and dwell in isolated places, where they can perform terrible magical experiments without interference.'
           ]
         }
       ]
